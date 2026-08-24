4c877384511b08c19971d783fef6a62799989cf5
mao2009 <39354512+mao2009@users.noreply.github.com>
2026-08-24 09:43:00 +0900

    feat(cpu): add branch target calculation (#79)
    Co-authored-by: Hobart <d.sasakura@llc-com.co.jp>

---
+++ src/PSXRecomp.Core/Cpu/R3000aBranchSemantics.cs
@@ -0,0 +1,37 @@
+using PSXRecomp.Architecture;
+
+namespace PSXRecomp.Core.Cpu;
+
+[Domain]
+public static class R3000aBranchSemantics
+{
+    public static bool TryGetBranchTarget(in R3000aInstruction instruction, uint pc, out uint target)
+    {
+        switch (instruction.Opcode)
+        {
+            case R3000aOpcode.Beq:
+            case R3000aOpcode.Bne:
+            case R3000aOpcode.Blez:
+            case R3000aOpcode.Bgtz:
+            case R3000aOpcode.Bltz:
+            case R3000aOpcode.Bgez:
+            case R3000aOpcode.Bltzal:
+            case R3000aOpcode.Bgezal:
+                break;
+            default:
+                target = 0;
+                return false;
+        }
+
+        var offsetOperand = instruction.GetOperand(instruction.OperandCount - 1);
+        if (offsetOperand.Kind != R3000aOperandKind.Immediate)
+        {
+            target = 0;
+            return false;
+        }
+
+        var scaledOffset = (uint)((int)(short)(ushort)offsetOperand.Value << 2);
+        target = unchecked(pc + 4u + scaledOffset);
+        return true;
+    }
+}
