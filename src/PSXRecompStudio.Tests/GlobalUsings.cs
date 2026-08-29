0c97dca3198f45f5ecc2b25ffebe9bbbdb7c44d0
mao2009 <mao2009@users.noreply.github.com>
2026-08-29 18:37:51 +0900

    test(gui): establish Avalonia headless GUI test foundation (Issue #178)

---
+++ src/PSXRecompStudio.Tests/GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using Avalonia.Controls;
+global using FluentAssertions;
+global using Xunit;
---
+++ src/PSXRecompStudio.Tests/MainWindowSmokeTests.cs
@@ -0,0 +1,35 @@
+using Avalonia.Headless.XUnit;
+using PSXRecompStudio.ViewModels;
+using PSXRecompStudio.Views;
+
+namespace PSXRecompStudio.Tests;
+
+public class MainWindowSmokeTests
+{
+    [AvaloniaFact]
+    public void MainWindow_DataContext_IsMainWindowViewModel()
+    {
+        var window = App.CreateMainWindow();
+        window.Show();
+
+        window.DataContext.Should().BeOfType<MainWindowViewModel>();
+    }
+
+    [AvaloniaFact]
+    public void AppName_Binding_Resolves()
+    {
+        var window = App.CreateMainWindow();
+        window.Show();
+
+        window.FindControl<TextBlock>("AppNameText")!.Text.Should().Be("PSXRecompStudio");
+    }
+
+    [AvaloniaFact]
+    public void Version_Binding_Resolves()
+    {
+        var window = App.CreateMainWindow();
+        window.Show();
+
+        window.FindControl<TextBlock>("VersionText")!.Text.Should().Be("0.1.0-dev");
+    }
+}
